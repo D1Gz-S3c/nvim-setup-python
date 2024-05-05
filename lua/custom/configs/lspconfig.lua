70405c05fa90681d28fc20cc4c739a189fd5e21e
D1Gz-S3c <diegohchackerone@gmail.com>
2024-05-05 16:11:43 -0300

    my dotfiles, perfect

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -6,11 +6,6 @@ local lspconfig = require("lspconfig")
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd" }
 
--- lspconfig.pyright.setup({
---   on_attach = on_attach,
---   capabilities = capabilities,
---   filetypes = {"python"},
--- })
 
 lspconfig.jedi_language_server.setup({
   capabilities = capabilities,
@@ -27,6 +22,3 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
--- 
--- lspconfig.pyright.setup { blabla}
