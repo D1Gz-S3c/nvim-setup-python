924bf670955869de2be48ce39e3ee21136970325
D1Gz-S3c <diegohchackerone@gmail.com>
2024-05-05 17:24:08 -0300

    attempt fix bruh:/

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -6,6 +6,11 @@ local lspconfig = require("lspconfig")
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd" }
 
+-- lspconfig.pyright.setup({
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   filetypes = {"python"},
+-- })
 
 lspconfig.jedi_language_server.setup({
   capabilities = capabilities,
@@ -22,3 +27,6 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+-- 
+-- lspconfig.pyright.setup { blabla}
